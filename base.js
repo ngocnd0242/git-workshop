e2bf47857b862b8eaebc11d0be5fafa00722093f
Ngoc Ngo Dinh (ngocnd0242) <ngo.dinh.ngoc@sun-asterisk.com>
2020-12-17 11:43:58 +0700

    // Ticket A: Bổ xung thêm loại < 5000 là Mid small.

--- base.js
+++ base.js
@@ -2,9 +2,13 @@ function categories (amount) {
     let category = '';
     if (amount < 1000)
         category = 'small';
+    else if (amount < 5000)
+        category = 'Mid small'
     else if (amount < 10000)
         category = 'Mid';
     else if (amount < 100000)
         category = 'Large';
     return category;
 }
+
+// Ticket A: Bổ xung thêm loại < 5000 là Mid small.
